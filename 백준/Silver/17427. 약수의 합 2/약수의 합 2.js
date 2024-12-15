9af1eb677e7db9ca014707d40ca8b2bb15dddc07
로인 <151013952+bboroin@users.noreply.github.com>
2024-12-16 04:15:54 +0900

    [Silver II] Title: 약수의 합 2, Time: 216 ms, Memory: 11808 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let answer = 0;
+for (i = 1; i <= input; i++) {
+    answer += Math.floor(input / i) * i;
+}
+console.log(answer);
