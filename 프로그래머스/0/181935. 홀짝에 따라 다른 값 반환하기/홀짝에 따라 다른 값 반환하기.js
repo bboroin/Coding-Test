f3c87e703e230eb79d4d9893374a4500cccb7c1d
로인 <151013952+bboroin@users.noreply.github.com>
2024-12-18 15:56:37 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,15 +1,25 @@
-function solution(n) {
-    let answer = 0;
-    if (n%2 === 0)
-        for (i=1; i<n+1; i++) {
-            if (i%2 === 0)
-                answer +=i*i
-        }
-    else
-        for (i=1; i<n+1; i++) {
-            if (i%2 === 1)
-                answer +=i;
-        }
+// function solution(n) {
+//     let answer = 0;
+//     if (n%2 === 0)
+//         for (i=1; i<n+1; i++) {
+//             if (i%2 === 0)
+//                 answer +=i*i
+//         }
+//     else
+//         for (i=1; i<n+1; i++) {
+//             if (i%2 === 1)
+//                 answer +=i;
+//         }
             
-    return answer;
+//     return answer;
+// }
+
+function solution(n) {
+    let odd = 0;
+    let even = 0;
+
+    for(let i = 0; i <= n; i++) {
+        i%2===0 ? even += Math.pow(i, 2) : odd += i;
+    }
+    return n%2===0 ? even : odd;
 }
