8c2b71049737f8e81e97b43dbcf094865ef4e47b
로인 <151013952+bboroin@users.noreply.github.com>
2024-12-28 22:48:50 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,5 @@
 function solution(my_string, s, e) {
-    var answer = '';
-    let string1 = my_string.slice(0, s);
-    let string2 = my_string.slice(s, e+1).split('').reverse().join('');
-    let string3 = my_string.slice(e+1, my_string.length);
-    return string1+string2+string3;
+    let str = my_string.slice(s, e+1);
+    let new_str = str.split('').reverse().join('');
+    return my_string.replace(str, new_str);
 }
