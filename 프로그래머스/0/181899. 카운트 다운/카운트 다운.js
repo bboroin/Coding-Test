96174cb655bfd7bec4c54ad331bd4da191a315a2
로인 <151013952+bboroin@users.noreply.github.com>
2024-12-29 21:44:36 +0900

    [level 0] Title: 카운트 다운, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,7 +1,3 @@
 function solution(start_num, end_num) {
-    var answer = [];
-    for (let i=start_num; i>=end_num; i--) {
-        answer.push(i);
-    }
-    return answer;
+    return Array(start_num-end_num+1).fill(start_num).map((v, i)=>v-i);
 }
