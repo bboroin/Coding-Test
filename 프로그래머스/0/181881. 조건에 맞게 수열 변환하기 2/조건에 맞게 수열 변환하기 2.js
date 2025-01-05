b2b14ab17708734815b64ca8479baadac6b30648
로인 <151013952+bboroin@users.noreply.github.com>
2025-01-06 00:00:36 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 241.31 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,12 +1,18 @@
 function solution(arr) {
-    let i = 0;
-    while(true) {
-        const cur = arr.map(num=> (num>=50 && num%2==0) ? num/2 : (num<50 && num%2==1) ? num*2+1 : num);
-        const next = arr.every((v, i) => v === cur[i])
+    var answer = 0;
+    let before;
 
-        if (next) break;
-        i++;
-        arr = cur;
-    }
-    return i;
+    do {
+        before = [...arr]; // 이전 상태 저장
+
+        arr = arr.map(e => {
+            if (e >= 50 && e % 2 === 0) return e / 2; // 50 이상이고 짝수
+            if (e < 50 && e % 2 !== 0) return e * 2 + 1; // 50 미만이고 홀수
+            return e; // 나머지 경우
+        });
+
+        answer++;
+    } while (!arr.every((e, idx) => e === before[idx]));
+
+    return answer-1; // 반복 횟수 반환
 }
