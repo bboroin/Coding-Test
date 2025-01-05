12cff11f547b492adb395e78bf7b8f809d7a164a
로인 <151013952+bboroin@users.noreply.github.com>
2025-01-06 02:28:05 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let answer = 0;
+    if (num_list.length >= 11)
+        num_list.forEach((num) => answer+=num);
+    else {
+        answer = 1;
+        num_list.forEach((num) => answer*=num);
+    }
+    return answer;
+}
