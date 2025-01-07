69a8710466b58e139b8f56561b8fe140819e7241
로인 <151013952+bboroin@users.noreply.github.com>
2025-01-07 21:57:23 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,8 @@
+function solution(myString, pat) {
+    let answer = 0;
+    for (let i=0; i<myString.length; i++) {
+       if (myString.substring(i, i+pat.length) == pat)
+           answer++;
+    }
+    return answer;
+}
