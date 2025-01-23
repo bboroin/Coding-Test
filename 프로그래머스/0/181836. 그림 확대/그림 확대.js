b9a8ff26c080f5a386c580f4bf0df2e783bcda25
로인 <151013952+bboroin@users.noreply.github.com>
2025-01-23 16:38:53 +0900

    [level 0] Title: 그림 확대, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -1,10 +1,10 @@
 function solution(picture, k) {
     let answer = []
-    for (let i=0; i<picture.length; i++) {
-        let pic = [...picture[i]].map(v=>v.repeat(k)).join('');
+    picture.map(v=>{
+        let pic = [...v].map(el=>el.repeat(k)).join('');
         for (let j=0; j<k; j++) {
             answer.push(pic);
         }
-    }
+    })
     return answer;
 }
