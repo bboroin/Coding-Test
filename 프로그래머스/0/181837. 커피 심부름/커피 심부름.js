d0107882c5d0c6153e06256e1f2790c9b5b0eb73
로인 <151013952+bboroin@users.noreply.github.com>
2025-01-23 14:35:53 +0900

    [level 0] Title: 커피 심부름, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,7 +1,5 @@
 function solution(order) {
-    let ame = 0;
-    let cafe = 0;
-    let newOrder = order.map(v=>v=='anything'?v.replace('anything', 'americano'):v);
-    newOrder.map(v=>v.includes('americano')?ame++:cafe++);
-    return ame*4500+cafe*5000;
+    let result = 0;
+    order.forEach(v=>v.includes('latte')?result+=5000:result+=4500);
+    return result;
 }
