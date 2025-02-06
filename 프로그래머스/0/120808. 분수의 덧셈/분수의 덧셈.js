5aa2dfe9f1fc083a9b61537f3f01c61f707160e6
로인 <151013952+bboroin@users.noreply.github.com>
2025-02-06 17:46:07 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,15 +1,10 @@
-// 최대공약수 구하기
 function cal_gcd(a, b) {
-    return a % b === 0 ? b : cal_gcd(b, a % b);
-}
-
+        return a%b === 0 ? b : cal_gcd(b, a%b);
+    }
 function solution(numer1, denom1, numer2, denom2) {
-    var answer = [];
-
-    let numer = numer1 * denom2 + numer2 * denom1; // 분자
-    let denom = denom1 * denom2; // 분모
-    let gcd = cal_gcd(numer, denom); // 최대공약수     
-
-    answer = [numer / gcd, denom / gcd];
-    return answer;
+    let numer = numer1 * denom2 + numer2 * denom1;
+    let denom = denom1 * denom2; 
+    let gcd = cal_gcd(numer, denom);
+    
+    return [numer/gcd, denom/gcd];
 }
