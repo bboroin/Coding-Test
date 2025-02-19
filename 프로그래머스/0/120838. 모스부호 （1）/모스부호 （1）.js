57a28faf83f92de588f0e1e32bd82722d64395d9
로인 <151013952+bboroin@users.noreply.github.com>
2025-02-19 17:14:51 +0900

    [level 0] Title: 모스부호 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,13 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    let key = Object.keys(morse);
+    let value = Object.values(morse);
+    
+    return letter.split(' ').map(v=>value[key.indexOf(v)]).join('')
+}
