28645a448b05f344066a4bfee249129acef1c797
로인 <151013952+bboroin@users.noreply.github.com>
2025-02-25 04:02:05 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,7 +1,3 @@
 function solution(numbers, k) {
-    var answer = 2*(k-1);
-    while(numbers.length<answer) {
-        answer = answer%numbers.length
-    }
-    return numbers[answer];
+    return numbers[((2 * (k -1))) % numbers.length];
 }
