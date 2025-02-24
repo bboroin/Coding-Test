737d9659b081da972e9851ce18935d8fa3374f37
로인 <151013952+bboroin@users.noreply.github.com>
2025-02-25 02:09:49 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,4 +1,4 @@
 function solution(num_list, n) {
-    var answer = new Array(num_list.length/n).fill(0).map((_, i) => num_list.slice(i*n, i*n+n)) ;
+    var answer = new Array(num_list.length/n).fill(0).map((_, i) => num_list.splice(0, n)) ;
     return answer;
 }
