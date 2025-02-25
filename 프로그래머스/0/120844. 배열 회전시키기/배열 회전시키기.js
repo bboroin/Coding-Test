a5d5f829600a5f7f3558a6be0113929e36809339
로인 <151013952+bboroin@users.noreply.github.com>
2025-02-25 13:38:28 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,7 +1,7 @@
 function solution(numbers, direction) {
     if (direction == 'right') {
-        numbers.unshift(numbers.splice(-1));
+        numbers.unshift(numbers.pop());
     }
-    else numbers.push(numbers.splice(0, 1));
-    return numbers.flat();
+    else numbers.push(numbers.shift());
+    return numbers;
 }
