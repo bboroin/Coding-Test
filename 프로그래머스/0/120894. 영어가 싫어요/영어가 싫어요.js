ba06d9db372dca01b36a7e17a9b188d4181dd745
로인 <151013952+bboroin@users.noreply.github.com>
2025-03-06 18:12:41 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    const en = {
+        'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'
+    }
+    return Number(Object.keys(en).reduce((acc, cur)=>acc.replaceAll(cur, en[cur]), numbers));
+}
