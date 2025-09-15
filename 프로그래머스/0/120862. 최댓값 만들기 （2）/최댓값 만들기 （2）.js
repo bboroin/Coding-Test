68da8e5b7405766655e460e2f627afbddf3a2444
로인 <151013952+bboroin@users.noreply.github.com>
2025-09-15 10:13:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,5 +1,4 @@
 function solution(numbers) {
-    const max = numbers.sort((a, b)=>b-a).slice(0, 2).reduce((acc, cur)=>acc*cur);
-    const min = numbers.sort((a, b)=>b-a).slice(numbers.length-2, numbers.length).reduce((acc, cur)=>acc*cur);
-    return min > max ? min : max;
+    numbers.sort((a,b)=>b-a)
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length - 1]*numbers[numbers.length - 2]);
 }
