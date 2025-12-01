43fa7859c98139e5b86d0b9d273a945083db0afc
로인 <151013952+bboroin@users.noreply.github.com>
2025-12-01 17:51:08 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,8 @@
+function solution(food) {
+    let left = '';
+    for (let i=1; i<food.length; i++) {
+        left += i.toString().repeat(Math.floor(food[i] / 2))
+    }
+    const right = [...left].reverse().join("")
+    return left+"0"+right;
+}
